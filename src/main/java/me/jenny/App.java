e614fb92b9e012f59ce7951a8dd52cfb16538d73
yunhee.kim <kyh901126@nate.com>
2021-01-03 20:01:21 +0900

    Add annotation processor method feature

--- src/main/java/me/jenny/App.java
+++ src/main/java/me/jenny/App.java
@@ -5,7 +5,9 @@ package me.jenny;
  */
 public class App {
     public static void main(String[] args) {
-//        Moja moja = new MagicMoja();  // MagicMoja 클래스파일을 @Magic 이 생성해주고 있다.
-//        System.out.println("Hello World! " + moja.pullOut());
+        // annotation processor mvn clean install 후 여기서 mvn clean compile 해준 뒤,
+        // target/generated-sources/annotations Sources Root로 지정해주면 (패키지명) 짧게 쓸 수 있다.
+        Moja moja = new MagicMoja();  // MagicMoja 클래스파일을 @Magic 이 생성해주고 있다.
+        System.out.println("Hello World! " + moja.pullOut());
     }
 }
